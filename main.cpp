ce249361db93a212e0d3f2b9f548f97178d039d9
marieda92 <marie.deamorim@orange.fr>
2024-05-07 11:49:21 +0200

    merge

--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
 
 int main() {
+	//Exemple pour dilatation et erosion: 
+	string cheminImage = "C:/Users/marie/OneDrive - ISEP/Isep/A2/S2/Appli multimedia/TP/TP2/TP2/img/HappyFish.jpg";
+	Image inputImage(cheminImage);
+	inputImage.display();
+
+	char choix;
+	cout << "Voulez-vous faire une érosion (e) ou une dilatation (d) ? ";
+	cin >> choix;
+
+	bool isErosion;
+	if (choix == 'e' || choix == 'E') {
+		isErosion = true;
+	}
+	else {
+		isErosion = false;
+	}
+
+	int size;
+	cout << "Entrez la taille de l'érosion/dilatation (entre 0 et 30) : ";
+	cin >> size;
+
+	Image outputImage = Operation::DilatationOrErosion(inputImage, size, isErosion);
+	outputImage.display();
+
 
 	return 0;
 }
